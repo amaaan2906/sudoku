0b2819dea647e708f22f9181c7842d6ba36e5f51
Aman Tawakley <tawakley.aman@gmail.com>
2024-08-18 16:30:04 -0400

    hello world
    - bun init
    - hono init
    - tsconfig init
    - gitignore

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Hono } from 'hono'
+
+const app = new Hono({ strict: false })
+app.get('/', (c) => c.text('Hello Bun!'))
+
+export default {
+	port: process.env.PORT || 1337,
+	fetch: app.fetch,
+}
