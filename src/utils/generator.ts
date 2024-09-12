2eecd83a7405eda83ac71a848800bfd7b5918cc5
Aman Tawakley <tawakley.aman@gmail.com>
2024-09-11 20:20:35 -0400

    sudoku board generator and solver

---
+++ src/utils/generator.ts
@@ -0,0 +1,68 @@
+import solver, { solution_counter } from './solver'
+
+const log = function (msg: any) {
+    console.log(`<${new Date().toUTCString()}> [Generator] ${msg}`)
+}
+
+const LEVEL_MAP = ['easy', 'medium', 'hard']
+
+/**
+ * This function creates "holes" in the complete board while maintaining a unique solution.
+ *
+ * @param board 2d integer grid solved sudoku board
+ * @param tries number of failed attempts allowed
+ * @returns the removed values, generated board
+ */
+function poke_holes(board: number[][], tries: number = 15): any[] {
+    let holes = []
+    while (tries > 0) {
+        let row = Math.floor(Math.random() * 9)
+        let col = Math.floor(Math.random() * 9)
+        // restart loop if selected slot is already empty
+        if (board[row][col] == 0) continue
+
+        // backup value, incase the board doesn't have a unique solution
+        let val_bkp = board[row][col]
+        board[row][col] = 0
+        if (solution_counter(board) != 1) {
+            // count number of solutions to board
+            // if no/>1 solution, undo change
+            board[row][col] = val_bkp
+            tries -= 1
+        } else {
+            // only one solution to board
+            // create array of every deleted val
+            holes.push({
+                row: row,
+                col: col,
+                val: val_bkp,
+            })
+        }
+    }
+    return [board, holes]
+}
+
+/**
+ * Driver code for sudoku board generator
+ * 1. Create random complete sudoku board
+ * 2. Poke holes in complete board
+ * @param level integer difficulty for sudoku grid
+ * @param size integer AxA grid size (future functionality)
+ */
+export default function generator(level: number = -1, size: number = 9) {
+    if (level == -1) {
+        level = Math.floor(Math.random() * 3)
+        log(`Randomizing level difficulty: ${LEVEL_MAP[level]}`)
+    }
+    const zero_string =
+        '000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+    log('Generating unique board for solution')
+    let start_time = performance.now()
+    let solution: number[][] = solver(zero_string)
+    let end_time = performance.now()
+    log(`Unique board generated in ${end_time - start_time}ms`)
+    let tries = level == 0 ? 1 : level == 1 ? 8 : 20
+    let [game_board, removed_vals] = poke_holes(solution, tries)
+    log(`${removed_vals.length} values removed from board`)
+    return { game_board, solution, removed_vals }
+}
---
+++ src/utils/solver.ts
@@ -0,0 +1,235 @@
+// TODO: import CONFIG to set log level
+
+const log = function (msg: any) {
+    console.log(`<${new Date().toUTCString()}> [Solver] ${msg}`)
+}
+
+/**
+ * This function will check if the grid is completely filled in
+ * @param grid 9x9 board to check
+ * @returns true if the grid is complete
+ */
+function check_grid_complete(grid: number[][]): boolean {
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            if (grid[r][c] == 0) return false
+        }
+    }
+    return true
+}
+
+/**
+ * Fisher-Yates Sorting Algorithm.
+ * Used to shuffle [1->9] integer array for use in solver
+ * @returns shuffled integer array
+ */
+function shuffle_array(): number[] {
+    let res = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for (let i = res.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1))
+        ;[res[i], res[j]] = [res[j], res[i]]
+    }
+    return res
+}
+
+/**
+ * This function will check if placement of val at (row,col) in the grid is valid
+ * @param val integer value to check
+ * @param row integer row grid coordinate
+ * @param col integer column grid coordinate
+ * @param grid 2d integer array sudoku board
+ * @returns true if placement is valid
+ */
+function valid_placement(
+    val: number,
+    row: number,
+    col: number,
+    grid: number[][]
+) {
+    return (
+        row_valid(val, row, grid) &&
+        col_valid(val, col, grid) &&
+        box_valid(val, row, col, grid)
+    )
+}
+/**
+ * Helper function for valid_placement(). Check if val is unique in row
+ * @param val integer value to check
+ * @param row integer row grid coordinate
+ * @param grid 2d integer grid sudoku board
+ * @returns true if placement is valid in row
+ */
+function row_valid(val: number, row: number, grid: number[][]): boolean {
+    return grid[row].indexOf(val) == -1
+}
+
+/**
+ * Helper function for valid_placement(). Check if val is unique in column
+ * @param val integer value to check
+ * @param col integer column grid coordinate
+ * @param grid 2d integer grid sudoku board
+ * @returns true if placement is valid in column
+ */
+function col_valid(val: number, col: number, grid: number[][]): boolean {
+    for (let i = 0; i < 9; i++) {
+        if (grid[i][col] == val) return false
+    }
+    return true
+}
+
+/**
+ * Helper function for valid_placement(). Check if val is unique in its 3x3 sub-grid
+ * @param val integer value to check
+ * @param row integer row grid coordinate
+ * @param col integer column grid coordinate
+ * @param grid 2s integer grid sudoku board
+ * @returns true if placement if valid in sub-grid
+ */
+function box_valid(
+    val: number,
+    row: number,
+    col: number,
+    grid: number[][]
+): boolean {
+    // top-left corner coords of 3x3 box containing current (row,col)
+    let start_row_box: number = row - (row % 3)
+    let start_col_box: number = col - (col % 3)
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (grid[start_row_box + i][start_col_box + j] == val) return false
+        }
+    }
+    return true
+}
+
+/**
+ * Main sudoku board solver function using backtracking.
+ * @param grid 2d integer grid sudoku board
+ * @returns solved 2d integer grid sudoku board
+ */
+function backtrack_solver(grid: number[][]) {
+    if (check_grid_complete(grid)) return grid
+    for (let i = 0; i < 81; i++) {
+        // find empty slot
+        let row: number = Math.floor(i / 9)
+        let col: number = i % 9
+        if (grid[row][col] == 0) {
+            let num_list = shuffle_array()
+            for (let num of num_list) {
+                if (valid_placement(num, row, col, grid)) {
+                    grid[row][col] = num
+                    if (backtrack_solver(grid)) return grid
+                    grid[row][col] = 0
+                }
+            }
+            return undefined
+        }
+    }
+}
+
+/**
+ * Driver code for sudoku solver
+ * @param grid_string string representation of board
+ * @param type toggle solver algorithm (currently only supports backtracking)
+ * @returns
+ */
+export default function solver(
+    grid_string: string,
+    type: string = 'backtrack'
+): number[][] {
+    log('Initializing solver')
+    let size: number = Math.sqrt(grid_string.replaceAll(',', '').length)
+    let grid: number[][] = [...Array(size)].map(() => Array(size))
+    let index = 0
+    for (let r = 0; r < size; r++) {
+        for (let c = 0; c < size; c++) {
+            grid[r][c] = parseInt(grid_string.charAt(index++))
+        }
+    }
+    let res: any
+    if (type.toLowerCase() == 'backtrack') {
+        log('Solving grid using [Backtracking]')
+        let start_time = performance.now()
+        res = backtrack_solver(grid)
+        let end_time = performance.now()
+        log(`Grid Solved in ${end_time - start_time}ms`)
+    }
+    return res
+}
+
+/**
+ * This function counts the number of possible solutions for a given sudoku board
+ * @param grid 2d integer grid sudoku board
+ * @returns integer count of solutions
+ */
+export function solution_counter(grid: number[][]) {
+    var counter: number = 0
+    backtrack(grid)
+    return counter
+
+    function backtrack(grid: number[][]) {
+        if (check_grid_complete(grid)) {
+            counter++
+            return undefined
+        }
+        for (let i = 0; i < 81; i++) {
+            // find empty slot
+            let row: number = Math.floor(i / 9)
+            let col: number = i % 9
+            if (grid[row][col] == 0) {
+                for (let j = 1; j < 10; j++) {
+                    if (valid_placement(j, row, col, grid)) {
+                        grid[row][col] = j
+                        if (backtrack(grid)) return grid
+                        grid[row][col] = 0
+                    }
+                }
+                return undefined
+            }
+        }
+    }
+}
+
+// solver(
+//     '107005290000700084040009100010000470030000000006280500900501002060000009000608000'
+//     //187465293259713684643829157512936478834157926796284531978541362465372819321698745
+// )
+
+// let test = [
+//     [0, 5, 0, 0, 0, 7, 3, 8, 0],
+//     [0, 1, 3, 0, 8, 0, 0, 4, 0],
+//     [7, 0, 0, 2, 4, 0, 0, 5, 6],
+//     [3, 0, 0, 8, 0, 0, 0, 6, 0],
+//     [0, 7, 0, 0, 0, 6, 0, 0, 0],
+//     [9, 0, 6, 7, 5, 2, 0, 3, 0],
+//     [0, 0, 0, 0, 2, 0, 0, 7, 0],
+//     [1, 3, 0, 0, 0, 8, 0, 0, 5],
+//     [0, 6, 2, 0, 7, 0, 8, 0, 4],
+// ] // 6 solutions
+// log(solution_counter(test))
+
+// let test2 = [
+//     [0, 5, 0, 0, 0, 7, 3, 8, 0],
+//     [0, 1, 3, 0, 0, 0, 0, 4, 7],
+//     [7, 0, 0, 2, 0, 0, 0, 5, 0],
+//     [0, 0, 0, 8, 0, 0, 0, 6, 0],
+//     [0, 7, 0, 0, 0, 6, 0, 0, 0],
+//     [9, 0, 6, 0, 5, 2, 0, 0, 0],
+//     [0, 0, 0, 0, 2, 0, 0, 7, 0],
+//     [0, 3, 0, 0, 0, 8, 0, 0, 5],
+//     [0, 6, 2, 0, 7, 0, 8, 0, 4],
+// ] //16
+// log(solution_counter(test2))
+
+// let test3 = [
+//     [0, 5, 0, 0, 0, 7, 3, 8, 0],
+//     [0, 1, 0, 0, 0, 0, 0, 4, 7],
+//     [7, 0, 0, 2, 4, 0, 0, 5, 0],
+//     [0, 0, 0, 8, 0, 0, 0, 6, 0],
+//     [0, 7, 0, 0, 0, 6, 0, 0, 0],
+//     [9, 0, 0, 0, 5, 2, 0, 0, 0],
+//     [0, 0, 0, 0, 2, 0, 0, 7, 0],
+//     [0, 3, 0, 0, 0, 8, 0, 0, 5],
+//     [0, 6, 2, 0, 7, 0, 8, 0, 4],
+// ] // 1
+// log(solution_counter(test3))
