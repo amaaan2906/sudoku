d406357c9141813534f26453b97c2351a28f95d2
Aman Tawakley <tawakley.aman@gmail.com>
2024-09-11 20:19:56 -0400

    missed indent change

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,17 +2,18 @@ import dotenvExtended from 'dotenv-extended'
 import dotenvParseVariables from 'dotenv-parse-variables'
 
 const env = dotenvExtended.load({
-  path: `src/config/.env.${process.env.APP_ENV}`,
-  defaults: 'src/config/.env.defaults',
-  schema: 'src/config/.env.schema',
-  includeProcessEnv: true,
-  silent: false,
-  errorOnMissing: true,
-  errorOnExtra: true,
+    path: `src/config/.env.${process.env.APP_ENV}`,
+    defaults: 'src/config/.env.defaults',
+    schema: 'src/config/.env.schema',
+    includeProcessEnv: true,
+    silent: false,
+    errorOnMissing: true,
+    errorOnExtra: true,
 })
 
 const parsed_env = dotenvParseVariables(env)
 
+// TODO: add log level config
 interface Config {}
 
 const config: Config = {}
