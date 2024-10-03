0feb734a7f4dbdcdc04198946e556e12d70aeba0
Aman Tawakley <tawakley.aman@gmail.com>
2024-10-02 22:28:09 -0400

    move cron job
    - game gen cron job moved to public/api file

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,6 @@ const log = function (msg: any) {
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 
-import { CronJob } from 'cron'
-
-import { createHmac } from 'node:crypto'
-
 const app = new Hono({ strict: false })
 app.use(logger())
 
@@ -23,24 +19,3 @@ export default {
     port: process.env.PORT || 1337,
     fetch: app.fetch,
 }
-
-CronJob.from({
-    cronTime: '0 5 * * *',
-    onTick: () => {
-        log('test')
-    },
-    start: true,
-    timeZone: 'Etc/GMT',
-})
-
-// setInterval(() => {
-//     const secret = 'ilovesudoku'
-//     const hash = createHmac('SHA1', secret)
-//         .update(
-//             '107005290000700084040009100010000470030000000006280500900501002060000009000608000'
-//         )
-//         .digest('hex')
-//     console.log(hash)
-//     // Prints:
-//     //   c0fa1bc00531bd78ef38c628449c5102aeabd49b5dc3a2a516ea6ea959d6658e
-// }, 1000)
--- src/routes/public/index.ts
+++ src/routes/public/index.ts
@@ -1,10 +1,10 @@
 import { Hono } from 'hono'
-
-const public_api = new Hono().basePath('/api')
+import { CronJob } from 'cron'
 
 import generator from '../../utils/generator'
-
 import solver from '../../utils/solver'
+
+const public_api = new Hono().basePath('/api')
 
 public_api.get('/', (c) => {
     return c.text("public api '/'")
@@ -19,6 +19,17 @@ public_api.get('/generate/:level?', (c) => {
 
 public_api.get('solve/:board_string', (c) => {
     return c.text(solver(c.req.param('board_string'), true).toString())
+})
+
+CronJob.from({
+    cronTime: '0 5 * * *',
+    onTick: () => {
+        for (let i = 0; i < 10; i++) {
+            let game = generator()
+        }
+    },
+    start: true,
+    timeZone: 'Etc/GMT',
 })
 
 export default public_api
